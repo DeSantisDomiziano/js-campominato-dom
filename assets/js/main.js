8b7ef90150985388f25761d687548c78f3d2dd58
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-03-20 17:36:35 +0100

    add function

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,26 +5,15 @@ const dif_medium = 81
 const dif_hard = 49
 const MAX_BOMBS = 16
 
-btn_el.addEventListener('click', function() {
+
+function add_grid_cells() {
     const option_el = document.getElementById('difficulty').value
-    const bombs_position_one = [];
     if(option_el === 'easy') {
         main_el.innerHTML = ""
         for(let i = 0; i < dif_easy; i++) {
             const square_el = `<div class="col dif_easy">safe</div>`
             main_el.insertAdjacentHTML('beforeend', square_el)
         }
-        let squares = document.querySelectorAll('.row.py-3 > .col')
-        
-        while( bombs_position_one.length < MAX_BOMBS) {
-            const random_position = Math.floor(Math.random() * squares.length)
-            const bombs_position = random_position
-            const boms_decoration = squares[random_position].innerHTML = `<i class="fa-solid fa-bomb"></i>`
-            if(!bombs_position_one.includes(random_position)) {
-                bombs_position_one.push(bombs_position)
-            }
-        }
-        console.log(bombs_position_one);
 
     } else if (option_el === 'medium') {
         main_el.innerHTML = ""
@@ -39,13 +28,32 @@ btn_el.addEventListener('click', function() {
             main_el.insertAdjacentHTML('beforeend', square_el)
         }
     }
+}
+
+function add_bombs() {
+    const bombs_position_one = [];
+    let squares = document.querySelectorAll('.row.py-3 > .col')        
+        while( bombs_position_one.length < MAX_BOMBS) {
+            const random_position = Math.floor(Math.random() * squares.length)
+            const bombs_position = random_position
+            const boms_decoration = squares[random_position].innerHTML = `<i class="fa-solid fa-bomb"></i>`
+            if(!bombs_position_one.includes(random_position)) {
+                bombs_position_one.push(bombs_position)
+            }
+        }
+    
+}
+
+btn_el.addEventListener('click', function() {
+    const bombs_position_one = [];
+    add_grid_cells()
+    add_bombs()
     
     let squares_el = document.querySelectorAll('.row.py-3 > .col')
-    for(let i = 0; i < squares_el.length; i++) {
-        
+    for(let i = 0; i < squares_el.length; i++) {   
         let single_square = squares_el[i]
-
         single_square.addEventListener('click', function() {
+            console.log('CLICK')
             single_square.style.color = "white"
             if(bombs_position_one.includes(i)) {
                 alert('hai perso')
